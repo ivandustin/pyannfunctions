91ab589bb5dbed4f672cccf0393afdaef854ad6b
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-07-09 11:32:18 +0800

    Test activation gradient

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -1,8 +1,15 @@
-from jax.numpy import array, inf, array_equal
+from jax.numpy import array, inf, array_equal, ones_like, sum
 from annfunctions import activation
+from jax import grad
 
 def test():
     input    = array([-inf, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, inf])
     expected = array([ 0.0,  0.0,  0.0, 0.0, 0.0, 1.0, 1.5, 2.0, 2.0, 2.0, 2.0])
     actual   = activation(input)
+    assert array_equal(actual, expected)
+
+def test_gradient():
+    input    = array([-inf, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, inf])
+    expected = ones_like(input)
+    actual   = grad(lambda input: sum(activation(input)))(input)
     assert array_equal(actual, expected)
